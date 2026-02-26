cd6cdabffb80646d628de618efedb29e960a758f
Anomale <yandeoliveiralima2011@gmail.com>
2026-02-26 11:14:46 -0400

    Add Ponte.h with basic arithmetic function declarations

---
+++ Atividades/Criar conexão entre códigos/Ponte.h
@@ -0,0 +1,8 @@
+#pragma once
+
+int Soma(int Num1, int Num2);
+int Subtrair(int Num1, int Num2);
+int Multiplicar(int Num1, int Num2);
+double Divide(int Num1, int Num2);
+int Potencia(int Num1, int Num2);
+float Raiz(int Num1, int Num2);
